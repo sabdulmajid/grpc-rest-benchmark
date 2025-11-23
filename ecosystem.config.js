cf4dda7bb8c8cdcb2933ae8a78150ee3f5fed882
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2025-11-22 23:41:45 -0500

    add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "se464-lab",
+      script: "./dist/main.js",
+      args: "rest mysql",
+      instances: 2,
+      exec_mode: "cluster",
+      env: {
+        port: 3000,
+      },
+    },
+  ],
+};
